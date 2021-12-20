820f3d84af5316c300b4483db85459b0f86aab51
Sushil <Sushil@DESKTOP-MP9AJ1K>
2021-12-20 10:23:19 +0530

    new packge create

--- GitDemo1/src/com/DemoGit.java
+++ GitDemo1/src/com/DemoGit.java
@@ -1,11 +1,19 @@
 package com;
 
-public class DenoGit {
+import com.change.Change;
+
+public class DemoGit extends Change{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HellO");
 		System.out.println("Git Demo");
+		
+		DemoGit d = new DemoGit();
+		
+		d.m1();
+		d.m2();
+		d.m3();
 		
 	}
 
